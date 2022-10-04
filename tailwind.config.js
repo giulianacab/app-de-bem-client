cc6a4a5ef14b7ac4bc4a104eee753d981e4e26a6
Giuliana Cabral <cabral.giu@hotmail.com>
2022-10-04 14:56:54 -0300

    Trocando material por tailwind – 4out 14:56

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'orange': '#FF6648',
+      'pink': '#FF94AE',
+      'purple': 'A706CE',
+    },
+    fontFamily: {
+      sans: ['Paytone']
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
