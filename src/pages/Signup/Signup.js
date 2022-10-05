b0dc63d454e9b9ca4488a6d3871bb6fe423ff165
somemacsongs <pedromacenac@gmail.com>
2022-10-05 11:55:28 -0300

    whatever

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -39,8 +39,7 @@ export function Signup() {
     e.preventDefault();
 
     try {
-      const imgURL = await handleUpload();
-      await api.post("/users/signup", { ...form, img: imgURL });
+      await api.post("/users/signup", form);
 
       navigate("/login");
     } catch (error) {
@@ -133,9 +132,9 @@ export function Signup() {
                   Senha
                 </label>
                 <input
-                  id="confirm-password"
-                  name="confirm-password"
-                  type="confirm-password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
                   autoComplete="confirm-password"
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
@@ -150,7 +149,7 @@ export function Signup() {
 
             <div>
               <button
-                type="signup"
+                type="submit"
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-pink py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
