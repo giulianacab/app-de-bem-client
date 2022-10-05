d0b30865e8943fb73df23ff9ba56329ec5578b65
Lucas Vitro <lucasvitro9@gmail.com>
2022-10-05 08:17:17 -0400

    algumas coisas

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -151,7 +151,7 @@ export function Signup() {
             <div>
               <button
                 type="signup"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-pink py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-pink py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 </span>
