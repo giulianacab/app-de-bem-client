00a2e644bb5dbd11ccc18507f9b7e6b4a02f0591
somemacsongs <pedromacenac@gmail.com>
2022-10-05 12:02:19 -0300

    teste

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -135,11 +135,7 @@ export function Signup() {
                   id="confirmPassword"
                   name="confirmPassword"
                   type="password"
-<<<<<<< HEAD
-                  autoComplete="confirm-password"
-=======
                   autoComplete="confirmPassword"
->>>>>>> 8c09c2ffde0f7a7741edd507028f02a007aa0293
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Confirmar Senha"
