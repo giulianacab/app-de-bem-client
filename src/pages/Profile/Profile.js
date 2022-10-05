b4da4c4bc8ef1461784debd5a6e67175399658a8
Giuliana Cabral <cabral.giu@hotmail.com>
2022-10-05 14:21:12 -0300

    MyMoods c/ nome do usuário que logar - 5out 14:21

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -3,6 +3,7 @@ import { useContext } from "react";
 // import { api } from "../../api/api";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/authContext";
+import { Link } from "react-router-dom";
 
 export function Profile() {
   // const [user, setUser] = useState({ name: "", email: "" });
@@ -25,6 +26,27 @@ export function Profile() {
 
   return (
     <>
+    {/* HEADER */}
+    <header className="headerPages">
+      <div className="divHeaderPages">
+        <Link to ="/profile">
+        <img src="https://images2.imgbox.com/54/1f/hdwOJPTU_o.png" className="avatar" alt ="default avatar" />
+        </Link>
+        <div className="textHeaderPages">
+            <h3 className="titleHeaderPages">
+                PÁGINA SELECIONADA
+            </h3>
+            <h1 className="nameHeaderPages">
+            {loggedInUser.user.name}
+            </h1>
+        </div>
+        <Link to ="/profile">
+        <img src="https://svgshare.com/i/nAs.svg" className="configIcon" alt="config icon" />
+        </Link>
+      </div>
+    </header>
+
+
       <h1>{loggedInUser.user.name}</h1>
       <p>{loggedInUser.user.email}</p>
       <button onClick={handleLogOut}>Sair</button>
