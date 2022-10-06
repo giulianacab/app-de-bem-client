90113a05f13653b0e58ed4fd8d7928d692615da9
Giuliana Cabral <cabral.giu@hotmail.com>
2022-10-06 17:43:14 -0300

    Tentando ajustar o erro aqui

--- src/pages/Community/Community.js
+++ src/pages/Community/Community.js
@@ -1,3 +1,22 @@
-export function Community(){
+import { Header } from "../../components/Header/Header";
+import { Menu } from "../../components/Menu/Menu";
+import { Community } from "../../components/Community/Community";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/authContext";
+import { useContext } from "react";
+
+export function CommunityPage(){
+    const { loggedInUser } = useContext(AuthContext);
+
+    return (
+        <>
     
+        <Header where="COMUNIDADES" name={loggedInUser.user.name} />
+        
+        <Community />
+        
+        <Menu/>
+
+        </>
+    )
 }
